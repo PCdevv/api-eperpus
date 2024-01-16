0afbc1dd788cbacb8de75e1ef5b430f96abba5dc
PCdevv <putericitradev@gmail.com>
2024-01-16 23:30:33 +0700

    done konsep login admin

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Admin;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+    function register(Request $request) {
+        $request->validate([
+            "email"=>"required",
+            "password"=>"required",
+            "confirm_password"=>"required"
+        ]);
+
+        $userExist = Admin::where('email', $request->email)->first();
+        if ($userExist) {
+            return response()->json(["message" => "User sudah terdaftar"], 400);
+        }
+        $userData = Admin::create($request->all());
+        return response()->json($userData, 201);
+    }
+
+    function login(Request $request) {
+        $user = Admin::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(["message" => "Email atau Password salah"], 401);
+        }
+        // $password = Hash::check($request->password, $user->password);
+        // if (!$password) {
+        //     return response()->json(["message" => "Email atau Password salah"], 404);
+        // }
+
+        return response()->json(["token"=>"JWT nanti disini...."], 200);
+    }
+}
